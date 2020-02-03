56d826dab449993c91bf096fae902e0b3ad89594
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-03 16:00:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,7 +8,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     if BRANDS.include?(brand)
-      BRANDS.delete_at(BRANDS.index(brand))
+      BRANDS.delete(brand)
     else
       BRANDS << brand
     end
